503de2e7a57714ba9fc30cea651e0ff74e34125d
Pavlo Dmytrenko <mail@pavdmyt.com>
2017-10-29 23:01:00 +0200

    Basic implementation

---
+++ yaspin/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from .yaspin import spinner
+
+
+__all__ = ('spinner',)
---
+++ yaspin/yaspin.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+
+"""
+yaspin.yaspin
+~~~~~~~~~~~~~
+
+A lightweight spinner.
+"""
+
+import functools
+import itertools
+import sys
+import threading
+import time
+
+
+class Yaspin(object):
+
+    _default_seq = u'⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏'
+    _default_interval = 0.08
+
+    def __init__(self, text='', sequence='', interval=None):
+        self._text = text.strip()
+
+        if sequence and interval:
+            self._sequence = sequence
+            self._interval = interval
+        else:
+            self._sequence = self._default_seq
+            self._interval = self._default_interval
+
+        self._cycle = itertools.cycle(self._sequence)
+        self._stop_spin = None
+        self._spin_thread = None
+
+    def __repr__(self):
+        repr_ = u'<Spinner cycle={0!s}>'.format(self._sequence)
+
+        if sys.version_info.major == 2:
+            return repr_.encode(sys.stdout.encoding or 'utf-8')
+        return repr_
+
+    def __enter__(self):
+        self.start()
+        return self
+
+    def __exit__(self, exc_type, exc_val, traceback):
+        self.stop()
+        return False  # nothing is handled
+
+    def __call__(self, fn):
+        @functools.wraps(fn)
+        def inner(*args, **kwargs):
+            with self:
+                return fn(*args, **kwargs)
+        return inner
+
+    def start(self):
+        if sys.stdout.isatty():
+            self._hide_cursor()
+
+        self._stop_spin = threading.Event()
+        self._spin_thread = threading.Thread(target=self.spin)
+        self._spin_thread.setDaemon(True)
+        self._spin_thread.start()
+
+    def stop(self):
+        if self._spin_thread:
+            self._stop_spin.set()
+            self._spin_thread.join()
+
+        sys.stdout.write("\r")
+        self._clear_line()
+
+        if sys.stdout.isatty():
+            self._show_cursor()
+
+    def spin(self):
+        while not self._stop_spin.is_set():
+            spin_phase = next(self._cycle)
+            sys.stdout.write(u"\r{0} {1}".format(spin_phase, self._text))
+            sys.stdout.flush()
+            time.sleep(self._interval)
+            sys.stdout.write('\b')
+
+    @staticmethod
+    def _hide_cursor():
+        sys.stdout.write("\033[?25l")
+        sys.stdout.flush()
+
+    @staticmethod
+    def _show_cursor():
+        sys.stdout.write("\033[?25h")
+        sys.stdout.flush()
+
+    @staticmethod
+    def _clear_line():
+        sys.stdout.write('\033[K')
+
+
+def spinner(text='', sequence='', interval=None):
+    """Terminal spinner.
+
+    Can be used as a context manager or as decorator.
+    """
+    return Yaspin(text=text, sequence=sequence, interval=interval)
