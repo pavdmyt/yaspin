436396910b207bf5ee5fda192e0d665e003ef76e
Pavlo Dmytrenko <mail@pavdmyt.com>
2017-10-31 20:46:48 +0200

    add demo

--- examples/demo.py
+++ examples/demo.py
@@ -4,25 +4,21 @@ import time
 import yaspin
 
 
-def context_manager_default():
-    with yaspin.spinner(text="Braille"):
-        time.sleep(3)
-
-
-def context_manager_line():
-    with yaspin.spinner("Line", '-\\|/', 0.15):
-        time.sleep(3)
-
-
-@yaspin.spinner("Dots", u"⢄⢂⢁⡁⡈⡐⡠", 0.08)
-def decorated_function():
-    time.sleep(3)
-
-
+# Refer here for more spinner examples
+# https://github.com/sindresorhus/cli-spinners
 def main():
-    context_manager_default()
-    context_manager_line()
-    decorated_function()
+    params = [
+        ("Braille",  "",            None),
+        ("Line",     "-\\|/",       0.15),
+        ("Flip",    "___-``'´-___", 0.07),
+        ("Dot",     "⠁⠂⠄⠂",        0.12),
+        ("Dots",    u"⢄⢂⢁⡁⡈⡐⡠",    0.08),
+        ("Balloon",  ".oOo. ",      0.14),
+        ("Star",     "+x*",         0.08),
+    ]
+    for p in params:
+        with yaspin.spinner(*p):
+            time.sleep(3)
 
 
 if __name__ == '__main__':
---
+++ examples/ex1.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import time
+import yaspin
+
+
+def context_manager_default():
+    with yaspin.spinner(text="Braille"):
+        time.sleep(3)
+
+
+def context_manager_line():
+    with yaspin.spinner("Line", '-\\|/', 0.15):
+        time.sleep(3)
+
+
+@yaspin.spinner("Dots", u"⢄⢂⢁⡁⡈⡐⡠", 0.08)
+def decorated_function():
+    time.sleep(3)
+
+
+def main():
+    context_manager_default()
+    context_manager_line()
+    decorated_function()
+
+
+if __name__ == '__main__':
+    main()
