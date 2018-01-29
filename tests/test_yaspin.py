7796ae38aa7244ef8aa35a1c03a437a51caaa9b4
Pavlo Dmytrenko <mail@pavdmyt.com>
2018-01-29 21:03:15 +0200

    cleanup tests.test_yaspin

--- tests/test_yaspin.py
+++ tests/test_yaspin.py
@@ -17,66 +17,6 @@ from yaspin import Spinner, yaspin
 from yaspin.base_spinner import default_spinner
 
 
-ids = [
-    "default frames and interval",
-    "str text, str frames",
-    "unicode text, unicode frames (marked as unicode)",
-    "unicode text, str frames",
-    "str text, unicode frames",
-    "str text, List[] frames",
-    "str text, List[bytes] frames",
-    "str text, List[unicode] frames",
-    "str text, Tuple[] frames",
-    "str text, Tuple[bytes] frames",
-    "str text, Tuple[unicode] frames",
-]
-
-
-test_cases = [
-    # default frames and interval
-    ("", "", None),
-
-    # str text, str frames
-    ("Loading", "+x*", 80),
-
-    # unicode text, unicode frames (marked as unicode)
-    (u"Загрузка", u"⢄⢂⢁⡁⡈⡐⡠", 80),
-
-    # unicode text, str frames
-    ("ℙƴ☂ℌøἤ", "+x*", 80),
-
-    # str text, unicode frames
-    ("Loading", "⢄⢂⢁⡁⡈⡐⡠", 80),
-
-    #
-    # Iter frames
-    #
-
-    # TODO: add custom type that Implements iterable
-    #
-    # XXX: this is Bad, because different text inputs should
-    #      combine with different frames input
-
-    # str text, List[] frames
-    ("Empty list", [], 400),
-
-    # str text, List[bytes] frames
-    ("Bytes list", [b"\xf0\x9f\x8c\xb2", b"\xf0\x9f\x8e\x84"], 400),
-
-    # str text, List[unicode] frames
-    ("Unicode list", [u"🌲", u"🎄"], 400),
-
-    # str text, Tuple[] frames
-    ("Empty tuple", (), 400),
-
-    # str text, Tuple[bytes] frames
-    ("Bytes tuple", (b"\xf0\x9f\x8c\xb2", b"\xf0\x9f\x8e\x84"), 400),
-
-    # str text, Tuple[unicode] frames
-    ("Unicode tuple", (u"🌲", u"🎄"), 400),
-]
-
-
 @pytest.mark.parametrize("spinner, expected", [
     # None
     (None, default_spinner),
