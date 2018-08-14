c8010d620758e215556ee54a5500e4001f35744b
Pavlo Dmytrenko <mail@pavdmyt.com>
2018-08-14 14:15:28 +0300

    Update docstring

--- yaspin/yaspin.py
+++ yaspin/yaspin.py
@@ -508,7 +508,8 @@ def yaspin(
         red, green, yellow, blue, magenta, cyan, white.
 
     Available text highlights:
-        on_red, on_green, on_yellow, on_blue, on_magenta, on_cyan, on_white.
+        on_red, on_green, on_yellow, on_blue, on_magenta, on_cyan, on_white,
+        on_grey.
 
     Available attributes:
         bold, dark, underline, blink, reverse, concealed.
