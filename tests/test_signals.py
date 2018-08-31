b3e02167965ad570bd42c65496293528c90bc803
Pavlo Dmytrenko <mail@pavdmyt.com>
2018-08-31 21:57:04 +0300

    Basic tests for signals

---
+++ tests/test_signals.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+"""
+tests.test_signals
+~~~~~~~~~~~~~~~~~~
+
+Test Yaspin signals handling.
+"""
+
+from __future__ import absolute_import
+
+import functools
+import signal
+
+import pytest
+
+from yaspin import yaspin
+from yaspin.compat import iteritems
+
+
+def test_sigmap_setting(sigmap_test_cases):
+    sigmap = sigmap_test_cases
+    sp = yaspin(sigmap=sigmap)
+    if sigmap is None:
+        assert sp._sigmap == {}
+    else:
+        assert sp._sigmap == sigmap
+
+
+def test_sigmap_signals_get_registered(sigmap_test_cases):
+    sigmap = sigmap_test_cases
+    if not sigmap:
+        pytest.skip("{0!r} - unsupported case".format(sigmap))
+
+    sp = yaspin(sigmap=sigmap)
+    # If test fails, the spinner may infinitely stuck ignoring
+    # signals, except of SIGKILL; try-finally block ensures
+    # that spinner get stopped.
+    try:
+        sp.start()
+        for sig, sig_handler in iteritems(sigmap):
+            handler = signal.getsignal(sig)
+            is_partial = isinstance(handler, functools.partial)
+
+            if callable(sig_handler) and is_partial:
+                # Handler function is wrapped into ``partial`` and
+                # is accesible via ``func`` attribute.
+                assert sig_handler == handler.func
+            else:
+                # SIG_DFL and SIG_IGN cases
+                assert sig_handler == handler
+    finally:
+        sp.stop()
