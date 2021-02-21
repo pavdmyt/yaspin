55b288a8144e1d04c2b94377bf882b2fdd08f46f
Pavel Dmytrenko <dmytrenko.pavel@gmail.com>
2021-02-21 12:04:59 +0200

    license: bump year

--- yaspin/__init__.py
+++ yaspin/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# :copyright: (c) 2020 by Pavlo Dmytrenko.
+# :copyright: (c) 2021 by Pavlo Dmytrenko.
 # :license: MIT, see LICENSE for more details.
 
 from __future__ import absolute_import
