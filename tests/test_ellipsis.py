1aeea8d6cee9803512de641d8e703adc2dfdff69
Pavel Dmytrenko <pavdmyt@aiven.io>
2024-09-18 21:33:03 +0300

    ellipsis: handle edge case when terminal is too narrow
    Terminal size should be sufficient to include:
      frame_width + timer_width + ellipsis_width

--- tests/test_ellipsis.py
+++ tests/test_ellipsis.py
@@ -1,6 +1,9 @@
 import shutil
+import threading
 import time
 from unittest.mock import patch
+
+import pytest
 
 from yaspin import yaspin
 
@@ -32,3 +35,16 @@ def test_terminal_size_called_once(mock_get_terminal_size):
 
     long_running_function()
     mock_get_terminal_size.assert_called_once()
+
+
+@patch("shutil.get_terminal_size")
+def test_raises_when_term_is_too_small(mock_get_terminal_size):
+    mock_get_terminal_size.return_value.columns = 10
+    sp = yaspin(ellipsis="..." * 5)
+
+    with pytest.raises(ValueError):
+        # Some low-level trickery is needed here because Yaspin._compose_out()
+        # is called in a separate thread and the exception is not propagated.
+        # Pytest indicates that by warnings.warn(pytest.PytestUnhandledThreadExceptionWarning(msg))
+        sp._stop_spin = threading.Event()
+        sp._spin()
